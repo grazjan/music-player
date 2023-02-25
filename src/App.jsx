86967e2aa0ecc25360bd2607964535c317915b48
Gracjan <g.drozdek@ixtensa.de>
2023-02-25 22:15:00 +0100

    set basic routing, init pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Home, Artist, Playlist, Search, Login } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />}/>
+        <Route path="/search" element={<Search />}/>
+        <Route path="/playlist/:id" element={<Playlist />}/>
+        <Route path="/artist/:id" element={<Artist />}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+
+export { default as Home } from './Home';
+export { default as Artist } from './Artist';
+export { default as Playlist } from './Playlist';
+export { default as Search } from './Search';
+export { default as Login } from './Login';
